0c7720996bb7939ed9a8cbc5f95d091c97307f8a
Jasser Blanco <yaser.keny@gmail.com>
2023-01-24 22:52:33 -0500

    Primera del ejercicio hora 10:51pm

--- main.js
+++ main.js
@@ -1,6 +1,55 @@
 var app = new Vue({
   el: "#app",
   data: {
-    message: "Hello Vue!",
+    message: "Bienvenido",
+    nombres: "",
+    apellidos: "",
+    user: "",
+    date: "",
+    db: [],
+    newDb: "",
+    hoy: "",
+    fechaNacimiento: "",
+    edad: "",
+    meses: "",
+    id: "",
+  },
+
+  methods: {
+    guardarDatos() {
+      const calcularEdad = () => {
+        this.hoy = new Date();
+        this.fechaNacimiento = new Date(this.date);
+        this.edad = this.hoy.getFullYear() - this.fechaNacimiento.getFullYear();
+        this.meses = this.hoy.getMonth() - this.fechaNacimiento.getMonth();
+        if (
+          this.meses < 0 ||
+          (this.meses === 0 &&
+            this.hoy.getDate() < this.fechaNacimiento.getDate())
+        ) {
+          this.edad--;
+        }
+        console.log(this.edad);
+        return this.edad;
+      };
+
+      const getId = () => {
+        this.id = Date.now().toString(30);
+      };
+
+      getId();
+
+      calcularEdad();
+
+      this.db.push({
+        nombres: this.nombres,
+        apellidos: this.apellidos,
+        user: this.user,
+        edad: this.edad,
+        id: this.id,
+      });
+
+      console.log(this.db);
+    },
   },
 });
