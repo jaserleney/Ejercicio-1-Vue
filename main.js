86c88b9b4d4504b5ffc73482c226398a829b6428
Jasser Blanco <yaser.keny@gmail.com>
2023-01-25 10:05:05 -0500

    Primera hora del dia 25/01/2022 hora: 10:04 am

--- main.js
+++ main.js
@@ -13,54 +13,68 @@ var app = new Vue({
     edad: "",
     meses: "",
     id: "",
+    seen: false,
   },
+
+  computed: {},
 
   methods: {
     guardarDatos() {
-      const calcularEdad = () => {
-        this.hoy = new Date();
-        this.fechaNacimiento = new Date(this.date);
-        this.edad = this.hoy.getFullYear() - this.fechaNacimiento.getFullYear();
-        this.meses = this.hoy.getMonth() - this.fechaNacimiento.getMonth();
-        if (
-          this.meses < 0 ||
-          (this.meses === 0 &&
-            this.hoy.getDate() < this.fechaNacimiento.getDate())
-        ) {
-          this.edad--;
+      const verificarUsers = () => {
+        const existeUser = this.db.find((user) => user.user === this.user);
+        if (existeUser) {
+          alert("El nombre de usuario ya existe, por favor ingrese otro");
+          return false;
+        } else {
+          const calcularEdad = () => {
+            this.hoy = new Date();
+            this.fechaNacimiento = new Date(this.date);
+            this.edad =
+              this.hoy.getFullYear() - this.fechaNacimiento.getFullYear();
+            this.meses = this.hoy.getMonth() - this.fechaNacimiento.getMonth();
+            if (
+              this.meses < 0 ||
+              (this.meses === 0 &&
+                this.hoy.getDate() < this.fechaNacimiento.getDate())
+            ) {
+              this.edad--;
+            }
+            console.log(this.edad);
+            return this.edad;
+          };
+
+          const getId = () => {
+            const char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            let chartRamdon = "";
+            for (let i = 0; i < 4; i++) {
+              chartRamdon += char.charAt(
+                Math.floor(Math.random() * char.length)
+              );
+            }
+
+            let numRamdon = Math.round(
+              Math.random() * (9999 - 1000) + 1000
+            ).toString();
+
+            this.id = `${chartRamdon}${numRamdon}`;
+          };
+
+          getId();
+
+          calcularEdad();
+          this.db.push({
+            nombres: this.nombres,
+            apellidos: this.apellidos,
+            nombreCompleto: `${this.nombres} ${this.apellidos}`,
+            user: this.user,
+            edad: `${this.edad} años`,
+            id: this.id,
+          });
+
+          console.log(this.db);
         }
-        console.log(this.edad);
-        return this.edad;
       };
-
-      const getId = () => {
-        const char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        let chartRamdon = "";
-        for (let i = 0; i < 4; i++) {
-          chartRamdon += char.charAt(Math.floor(Math.random() * char.length));
-        }
-
-        let numRamdon = Math.round(
-          Math.random() * (9999 - 1000) + 1000
-        ).toString();
-
-        this.id = `${chartRamdon}${numRamdon}`;
-      };
-
-      getId();
-
-      calcularEdad();
-
-      this.db.push({
-        nombres: this.nombres,
-        apellidos: this.apellidos,
-        nombreCompleto: `${this.nombres} ${this.apellidos}`,
-        user: this.user,
-        edad: `${this.edad} años`,
-        id: this.id,
-      });
-
-      console.log(this.db);
+      verificarUsers();
     },
   },
 });
